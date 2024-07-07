4ef6420e78bfb4b7b1eb45fa786069cbd0f2cccb
DOM CHAROENYOS <dome@tel.co.th>
2024-07-07 23:08:40 +0700

    Update env.js

--- base/env.js
+++ base/env.js
@@ -2,4 +2,4 @@ import getConfig from "next/config";
 
 const { publicRuntimeConfig = {} } = getConfig()
 
-export const assetPrefix = publicRuntimeConfig.assetPrefix || 'https://jibchainlist.vercel.app/';
+export const assetPrefix = publicRuntimeConfig.assetPrefix || 'https://jibl2.com/';
