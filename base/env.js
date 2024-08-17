fd1a09fac14e6f3604b417c539f678d18056a2e6
Dome C <dome@tel.co.th>
2024-08-17 14:37:40 +0700

    fix url

--- base/env.js
+++ base/env.js
@@ -2,4 +2,4 @@ import getConfig from "next/config";
 
 const { publicRuntimeConfig = {} } = getConfig()
 
-export const assetPrefix = publicRuntimeConfig.assetPrefix || 'https://jibl2.com/';
+export const assetPrefix = publicRuntimeConfig.assetPrefix || 'https://list.list.jibl2.com/';
--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://jibl2.com/' : '',
+  assetPrefix: isProd ? 'https://list.jibl2.com/' : '',
   generateBuildId: async () => {
     return commitHash;
   },
   publicRuntimeConfig: {
-    assetPrefix: isProd ? 'https://jibl2.com/' : '',
+    assetPrefix: isProd ? 'https://list.jibl2.com/' : '',
   }
 }
